172951de46f004e25c5e4ff9aaabb65a9d026de1
unknown <cchisomfrancis@gmail.com>
2022-06-04 17:40:35 +0100

    Json

--- todoapp/views.py
+++ todoapp/views.py
@@ -3,6 +3,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import TaskSerializer
 from .models import Task
+from django.http import JsonResponse
 # Create your views here.
 
 
